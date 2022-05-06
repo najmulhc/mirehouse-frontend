103d3633516d6b55dc6d70320719c7d6d9613eb2
kridi32 <kridi32@outlook.com>
2022-05-07 00:28:13 +0600

    file setup and packages installation done

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBm24l3TxTl-FXFbNInGhR-P4cxhxLbOms",
+  authDomain: "the-summit-gear.firebaseapp.com",
+  projectId: "the-summit-gear",
+  storageBucket: "the-summit-gear.appspot.com",
+  messagingSenderId: "765634140613",
+  appId: "1:765634140613:web:d62feab4f2d76e90f8d3ac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
