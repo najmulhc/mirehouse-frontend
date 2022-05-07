a95725ff1e7ce607ef782f4ea9d457179afbff59
kridi32 <kridi32@outlook.com>
2022-05-07 09:41:37 +0600

    header complete with name and staffs

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home";
 import Inventory from "./Pages/Inventory/Inventory";
+import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
     <div className="App">
-        <Header></Header>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/inventory" element={<Inventory/>} />
-
-        </Routes>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/inventory" element={<Inventory />} />
+      </Routes>
+      
     </div>
   );
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react"; 
-import{ImCross, ImMenu} from"react-icons/im"
+import React, { useState } from "react";
+import { GiMountainClimbing } from "react-icons/gi";
+import { ImCross, ImMenu } from "react-icons/im";
 import { Link } from "react-router-dom";
+
 export default function Header({ fixed }) {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
@@ -8,12 +10,12 @@ export default function Header({ fixed }) {
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-green-600  mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-              href="#pablo"
+            <Link
+              className="text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+              to='/'
             >
-              yellow Tailwind Starter Kit
-            </a>
+             <span> <GiMountainClimbing className="inline text-4xl"></GiMountainClimbing> Summit gear</span>
+            </Link>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
@@ -29,7 +31,7 @@ export default function Header({ fixed }) {
             }
             id="example-navbar-danger"
           >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto items-center">
               <li className="nav-item">
                 <Link to='/'
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
@@ -51,7 +53,7 @@ export default function Header({ fixed }) {
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                   href="#pablo"
                 >
-                  <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Inventory</span>
+                  <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2 bg-white hover:bg-blue-dark text-green-600 font-bold py-2 px-4 rounded">Inventory</span>
                 </Link>
               </li>
               
