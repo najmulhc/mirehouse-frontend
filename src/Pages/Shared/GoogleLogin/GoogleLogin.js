f02eef8d6d26f9adcc05b91654e6d4acf68cf2c4
kridi32 <kridi32@outlook.com>
2022-05-09 11:53:24 +0600

    google login is done

--- src/Pages/Shared/GoogleLogin/GoogleLogin.js
+++ src/Pages/Shared/GoogleLogin/GoogleLogin.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { Bars } from "react-loader-spinner";
 import { useNavigate } from "react-router-dom";
 import auth from "../../../firebase.init";
 
@@ -12,14 +13,21 @@ const GoogleLogin = () => {
   if (user) {
     navigate("/");
   }
+  if(loading){
+    return <div className="w-full h-[160px] flex items-center justify-center"><Bars
+    color="#F9AA4B"
+    height={110}
+    width={110}
+    ariaLabel="three-circles-rotating"
+  /></div>}
   return (
     <div>
-      <div className="flex items-center w-5/6 my-2 mx-auto">
-        <hr className="border border-green-600 w-full mr-1" /> <h1>or</h1>
-        <hr className="border border-green-600 w-full ml-1" />
+      <div className="flex items-center w-full my-2 mx-auto">
+        <hr className="border border-komola w-full mr-1" /> <h1 className="text-komola">or</h1>
+        <hr className="border border-komola w-full ml-1" />
       </div>
       <button
-        className="w-5/6  bg-green-600 text-white py-1 text-xl"
+        className="text-white  rounded-full  font-bold py-2 px-20  hover:text-komola bg-komola border hover:bg-white border-komola  my-2 mx-auto "
         type="submit"
         onClick={googleLogin}
       >
